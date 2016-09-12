38d4f0ba562d50f8c47362b52d358ca16563b540
Jonathan Gnagy <jgnagy@knuedge.com>
2016-09-12 11:29:26 -0700

    Got search working

--- lib/cratus/ldap.rb
+++ lib/cratus/ldap.rb
@@ -14,6 +14,57 @@ module Cratus
       )
     end
 
+    def self.connect
+      validate_ldap_connection
+      @@ldap_connection.bind
+      @@ldap_bound = true
+    end
+
+    def self.search(filter, options = {})
+      validate_ldap_connection
+      validate_ldap_bound
+      validate_search_options(options)
+
+      attrs = options.key?(:attrs) ? options[:attrs] : []
+      scope = options.key?(:scope) ? options[:scope] : 'subtree'
+
+      scope_class = case scope.to_s
+                    when 'subtree','recursive','whole_subtree'
+                      Net::LDAP::SearchScope_WholeSubtree
+                    when 'single','single_level'
+                      Net::LDAP::SearchScope_SingleLevel
+                    when 'object','base_object'
+                      Net::LDAP::SearchScope_BaseObject
+                    else
+                      fail "Invalid LDAP Scope!"
+                    end
+      
+      @@ldap_connection.search(
+        base: options[:basedn],
+        filter: filter,
+        scope: scope_class,
+        attributes: [*attrs].map(&:to_s)
+      ).compact
+    end
+
+    # Validation Methods
+
+    def self.validate_ldap_bound
+      raise "LDAP Not Connected" unless @@ldap_bound
+    end
+
+    def self.validate_ldap_connection
+      raise "No LDAP Connection" unless @@ldap_connection
+    end
+
+    def self.validate_search_options(options)
+      raise "Invalid Options" unless options.respond_to?(:key?)
+
+      [:basedn].each do |key|
+        raise "Missing Option: #{key}" unless options.key?(key)
+      end
+    end
+
     def self.validate_connection_options(options)
       raise "Invalid Options" unless options.respond_to?(:key?)
 
