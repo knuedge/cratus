6cc5c0095c316342b251f45fc4f27ba76faccd70
Jonathan Gnagy <jgnagy@knuedge.com>
2016-09-12 10:45:35 -0700

    Adding some validations to LDAP options

--- lib/cratus/ldap.rb
+++ lib/cratus/ldap.rb
@@ -1,6 +1,7 @@
 module Cratus
   module LDAP
     def self.connection(options = {})
+      validate_connection_options(options)
       @@ldap_connection ||= Net::LDAP.new(
         host: options[:host],
         port: options[:port],
@@ -11,6 +12,14 @@ module Cratus
           password: options[:password]
         }
       )
+    end
+
+    def self.validate_connection_options(options)
+      raise "Invalid Options" unless options.respond_to?(:key?)
+
+      [:host, :port, :basedn, :username, :password].each do |key|
+        raise "Missing Option: #{key}" unless options.key?(key)
+      end
     end
   end
 end
