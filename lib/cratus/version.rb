dafb0498719c58f76ea4a49cd826e840430e7eb6
Jonathan Gnagy <jgnagy@knuedge.com>
2016-10-07 12:27:31 -0700

    More powerful and more better version bump!

--- lib/cratus/version.rb
+++ lib/cratus/version.rb
@@ -2,7 +2,7 @@ module Cratus
   def self.version
     major = 0 # Breaking, incompatible releases
     minor = 2 # Compatible, but new features
-    patch = 0 # Fixes to existing features
+    patch = 1 # Fixes to existing features
     [major, minor, patch].map(&:to_s).join('.')
   end
 end
