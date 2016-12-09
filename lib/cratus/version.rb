118c453b42dda47611ff503be1b6851da3ae77af
Jonathan Gnagy <jgnagy@knuedge.com>
2016-12-08 16:50:47 -0800

    Adding missing dependency for cratus-compare Version bump

--- lib/cratus/version.rb
+++ lib/cratus/version.rb
@@ -3,7 +3,7 @@ module Cratus
   def self.version
     major = 0 # Breaking, incompatible releases
     minor = 2 # Compatible, but new features
-    patch = 6 # Fixes to existing features
+    patch = 7 # Fixes to existing features
     [major, minor, patch].map(&:to_s).join('.')
   end
 end
