8be7fcefd3901b880d8ce8eb8994f78ff90f6db5
Daniel Schaaff <dschaaff@knuedge.com>
2017-03-27 20:36:22 -0600

    bump version for release

--- lib/cratus/version.rb
+++ lib/cratus/version.rb
@@ -3,7 +3,7 @@ module Cratus
   def self.version
     major = 0 # Breaking, incompatible releases
     minor = 3 # Compatible, but new features
-    patch = 7 # Fixes to existing features
+    patch = 8 # Fixes to existing features
     [major, minor, patch].map(&:to_s).join('.')
   end
 end
